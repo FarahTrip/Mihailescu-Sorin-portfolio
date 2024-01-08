3a5672b8c9f1181de6a628b463ec71b8a9de672d
Sorin Mihailescu <121116598+FarahTrip@users.noreply.github.com>
2024-01-08 12:46:20 +0200

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,71 @@
+export const NAME = 'Mihailescu Sorin Daniel';
+
+export const DESIGNATION = `Software developer | AI Enthusiast | Musician`;
+
+export const DETAILS = {
+  Residence: 'Romania',
+  City: 'Bucuresti',
+  Age: '25',
+};
+
+export const SKILLS = [
+  {
+    title: 'NestJs (back-end)',
+    level: '75%',
+  },
+  {
+    title: 'NextJs (front-end)',
+    level: '70%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Leadership',
+    level: '81%',
+  },
+  {
+    title: 'C# & .NET, ASP.NET',
+    level: '65%',
+  },
+  
+  {
+    title: 'Machine learning',
+    level: '32%',
+  },
+];
+
+export const TECH_STACK = [
+  'Typescript',
+  'ReactJS',
+  'JavaScript',
+  'NextJS',
+  'NestJs',
+  'NodeJS',
+  'CSS',
+  'REST API',
+  'TailwindCSS',
+  'Supabase',
+  'PostgreSql',
+  'Material UI',
+  'Next UI',
+  'Python',
+  'SCSS',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+];
+
+export const CONTACTS = {
+  EMAIL: 'sorin.mihailescu@outlook.com',
+  PHONE: '+40 0720590096',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/FarahTrip',
+  LINKEDIN: 'https://www.linkedin.com/in/mihailescu-sorin-daniel-46a370189/',
+  TWITTER: '',
+  FACEBOOK: '',
+};
