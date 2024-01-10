f39ac6da7d765f18718d5e54f8d1b3e8f22f090f
Sorin Mihailescu <121116598+FarahTrip@users.noreply.github.com>
2024-01-11 00:34:05 +0200

    fixing images part 2

--- components/Common/Projects/AudioFeatures.jsx
+++ components/Common/Projects/AudioFeatures.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
+import trippin4 from "../../../public/images/trippin4.png"
+import trippin5 from "../../../public/images/trippin5.png"
+import trippin6 from "../../../public/images/trippin6.png"
 
 const AudioFeatures = () => {
   return (
@@ -7,13 +10,13 @@ const AudioFeatures = () => {
       <p>
 Initially, I conducted an analysis to identify essential features for developing a robust content-based recommendation system, drawing insights from Spotify's practices. Subsequently, I carefully selected these features that are really important. I also found the FMA dataset which had a couple of songs with the features that I need in an excel file.</p>
 <div className='w-full flex justify-center gap-40'>
-<Image className='flex w-80' width={1000} height={1000} src={'/images/trippin4.png'}></Image>
-<Image className='flex w-80' width={1000} height={1000} src={'/images/trippin5.png'}></Image>
+<Image className='flex w-80' width={1000} height={1000} src={trippin4}></Image>
+<Image className='flex w-80' width={1000} height={1000} src={trippin5}></Image>
 
 </div>
 <p>
 I meticulously prepared the data, extracting a set of audio features that serve as the foundation for predicting the necessary values.</p>
-<Image className='flex ' width={1000} height={1000} src={'/images/trippin6.png'}></Image>
+<Image className='flex ' width={1000} height={1000} src={trippin6}></Image>
     </div>
   )
 }
--- components/Common/Projects/Trippin.jsx
+++ components/Common/Projects/Trippin.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import { CopyBlock,dracula } from "react-code-blocks"; 
+import trippin3 from "../../../public/images/trippin3.png"
+import trippin2 from "../../../public/images/trippin2.gif"
 
 
 const Trippin = () => {
@@ -8,10 +10,10 @@ const Trippin = () => {
     <div className='mt-4 gap-4 flex flex-col'>
         <p>
 This illustrates the initial development phase, highlighting the incorporation of a global audio player. The implementation ensures users can enjoy music seamlessly throughout the entire application.</p>
-        <Image width={1000} height={100} className='w-full' src={'/images/trippin2.gif'}></Image>
+        <Image width={1000} height={100} className='w-full' src={trippin2}></Image>
         <p>I managed authentication for both front and backend systems, initiating role-based authorization for producers and artists.</p>
         <p> Additionally, I devised a system to validate song ownership by leveraging ACRCloud, determining whether the song is already online or not.</p>
-        <Image width={1000} height={100} className='w-full' src={'/images/trippin3.png'}></Image>
+        <Image width={1000} height={100} className='w-full' src={trippin3}></Image>
     </div>
   )
 }
