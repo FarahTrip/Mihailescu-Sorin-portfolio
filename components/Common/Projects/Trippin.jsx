eb7b34d2d4ffbbbc418ee7f41e803926734df699
Sorin Mihailescu <121116598+FarahTrip@users.noreply.github.com>
2024-01-11 03:08:10 +0200

    adding the carrousel

--- components/Common/Projects/Trippin.jsx
+++ components/Common/Projects/Trippin.jsx
@@ -1,18 +1,22 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import { CopyBlock,dracula } from "react-code-blocks"; 
+import Carousel from '../Carousel';
 
 
 
 const Trippin = () => {
   return (
     <div className='mt-4 gap-4 flex flex-col'>
+              <Carousel data={['/images/trippin7.png', '/images/trippin.png', '/images/trippin8.png']}></Carousel>
         <p>
 This illustrates the initial development phase, highlighting the incorporation of a global audio player. The implementation ensures users can enjoy music seamlessly throughout the entire application.</p>
-        <Image width={1000} height={100} className='w-full' src={'/images/trippin3.png'}></Image>
+
+        <Image width={1000} height={1000} className='w-full' src={'/images/trippin2.gif'}></Image>
         <p>I managed authentication for both front and backend systems, initiating role-based authorization for producers and artists.</p>
         <p> Additionally, I devised a system to validate song ownership by leveraging ACRCloud, determining whether the song is already online or not.</p>
-        <Image width={1000} height={100} className='w-full' src={'/images/trippin2.gif'}></Image>
+        <Image width={1000} height={1000} className='w-full' src={'/images/trippin3.png'}></Image>
     </div>
   )
 }
--- components/Portfolio/PortfolioCard.jsx
+++ components/Portfolio/PortfolioCard.jsx
@@ -5,11 +5,14 @@ import Link from "next/link"
 const PortfolioCard = ({ data }) => {
     return (
         <div className="card_stylings overflow-hidden h-full">
+            <a href={data?.url}>
             <img
                 src={data?.image}
                 alt="portfolio img"
                 className="w-full object-cover opacity-30 h-32 sm:h-48 md:h-64"
             />
+            </a>
+        
             <div id="arrow" className="py-2 px-6 card_stylings hover:-translate-y-10 transition-all ease-in-out duration-500">
                 <div className="flex justify-between p-0 m-0 ">
                     <h3 className="mr-2 underline italic font-semibold pt-2 text-2xl text-Snow leading-tight sm:leading-normal">
